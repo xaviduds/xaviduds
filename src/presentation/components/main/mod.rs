4d147364e07a613700133f5dfb8d17198743b716
Eduardo de Melo Xavier <xaviduds@gmail.com>
2025-05-19 21:57:04 -0300

    Removed experiences

--- src/presentation/components/main/mod.rs
+++ src/presentation/components/main/mod.rs
@@ -1,20 +1,16 @@
-pub mod experience;
 pub mod projects;
 pub mod stack;
 
-use crate::presentation::components::main::{experience::experience, projects::projects};
+use crate::presentation::components::main::projects::projects;
 use maud::html;
 use stack::stack;
 
 pub fn comp_main() -> String {
     html!(
-    main class="row" {
-        div class="column" {
-            (experience())
+        main class="row" {
             (projects())
+            (stack())
         }
-        (stack())
-    }
     )
     .0
 }
