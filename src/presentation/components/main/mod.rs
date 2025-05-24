f59ef118c7e6dda7ebf162c5804c69c3b894856b
Eduardo de Melo Xavier <xaviduds@gmail.com>
2025-05-24 09:28:55 -0300

    Removed projects+experience

--- src/presentation/components/main/mod.rs
+++ src/presentation/components/main/mod.rs
@@ -1,14 +1,11 @@
-pub mod projects;
 pub mod stack;
 
-use crate::presentation::components::main::projects::projects;
 use maud::html;
 use stack::stack;
 
 pub fn comp_main() -> String {
     html!(
         main class="row" {
-            (projects())
             (stack())
         }
     )
--- src/schema/experience.rs
+++
@@ -1,7 +0,0 @@
-pub struct Experience {
-    pub company: &'static str,
-    pub start: &'static str,
-    pub end: &'static str,
-    pub title: &'static str,
-    pub details: &'static str,
-}
